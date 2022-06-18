a8b114bd66428b7128042110e373b77883ac85fb
DasherDo <josh@hessjd.com>
2022-06-18 11:27:01 -0500

    range slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import VisualizeArray from './components/Main/VisualizeArray'
 import React from 'react';
+import Header from './components/Header/Header'
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <VisualizeArray />
     </div>
   );
