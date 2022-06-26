0160105878e6185396264e49afa98c7f7df61f8b
DasherDo <josh@hessjd.com>
2022-06-26 12:44:06 -0500

    merge

--- src/components/Header/Slider.js
+++ src/components/Header/Slider.js
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from 'react';
 import './header.css'
 
 function Slider () {
+
+    const [val, setVal] = useState(25);
+
+    useEffect(()=> {
+        setVal(val)
+    },[val]);
+
     return (
         <div className='slider'>
-            <input type='range' min='1' max='100' value='50'></input>
+            <input type='range' min='1' max='50'></input>
+            {val}
         </div>
     )
 };
