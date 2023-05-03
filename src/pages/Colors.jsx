16f0bb178447588eb7c580b3404492152d0ebd77
Josh <josh@hessjd.com>
2023-05-03 14:15:35 -0500

    update

--- src/pages/Colors.jsx
+++ src/pages/Colors.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './colors.css';
 
 function Colors() {
+	const CANVAS_WIDTH = 350;
+	const CANVAS_HEIGHT = 350;
+
 	const shuffleArray = (arr) => {
 		for (let i = arr.length - 1; i > 0; i--) {
 			// Pick a random index from 0 to i inclusive
@@ -13,24 +16,12 @@ function Colors() {
 		}
 		return arr;
 	};
-	const draw = () => {
+
+	// Gets context object and pixel array of current canvas
+	const getCanvas = () => {
 		let c = document.getElementById('canv');
 		let ctx = c.getContext('2d');
-
-		let grd = ctx.createLinearGradient(0, 0, 255, 255);
-		grd.addColorStop(0, 'blue');
-		grd.addColorStop(1, 'red');
-
-		ctx.fillStyle = grd;
-		ctx.fillRect(20, 20, 150, 100);
-	};
-
-	const getData = () => {
-		// Gets canvas element
-		let c = document.getElementById('canv');
-		let ctx = c.getContext('2d');
-		let imageData = ctx.getImageData(20, 20, 150, 100);
-
+		let imageData = ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 		// Separates out every pixel from Image Data object
 		// pixels is a 2d arrays, where every index is an Uint8ClampedArray
 		let pixels = [];
@@ -38,28 +29,69 @@ function Colors() {
 			const cur = [imageData.data.slice(i, i + 4)];
 			pixels.push(cur);
 		}
-		// Reverses the pixels
-		rev(pixels);
-	};
-
-	const rev = (pixels) => {
-		// Gets canvas element
-		let c = document.getElementById('canv');
-		let ctx = c.getContext('2d');
-		pixels = pixels.reverse();
-		pixels = shuffleArray(pixels);
-
-		// Loops through pixels and adds them to iData.
-		// Puts data into non Uint8ClampedArray
 		let iData = [];
 		for (let i = 0; i < pixels.length; i++) {
+			let cur = [];
 			for (let j = 0; j < pixels[i][0].length; j++) {
-				iData.push(pixels[i][0][j]);
+				cur.push(pixels[i][0][j]);
+			}
+			iData.push(cur);
+		}
+		console.log(iData);
+		return { ctx: ctx, pixelArray: iData };
+	};
+
+	const draw = () => {
+		let c = document.getElementById('canv');
+		let ctx = c.getContext('2d');
+		c.width = CANVAS_WIDTH;
+		c.height = CANVAS_HEIGHT;
+
+		ctx.scale(1, 1);
+
+		let grd = ctx.createLinearGradient(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+		grd.addColorStop(0, 'blue');
+		grd.addColorStop(1, 'red');
+
+		ctx.fillStyle = grd;
+		ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	};
+
+	// Redundant function
+	// const getData = () => {
+	// 	// Gets canvas element
+	// 	let c = document.getElementById('canv');
+	// 	let ctx = c.getContext('2d');
+	// 	let imageData = ctx.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+	// 	// Separates out every pixel from Image Data object
+	// 	// pixels is a 2d arrays, where every index is an Uint8ClampedArray
+	// 	let pixels = [];
+	// 	for (let i = 0; i < imageData.data.length; i += 4) {
+	// 		const cur = [imageData.data.slice(i, i + 4)];
+	// 		pixels.push(cur);
+	// 	}
+	// 	// Reverses the pixels
+	// 	rev(pixels);
+	// };
+
+	const rev = () => {
+		// Gets context and current pixel array from getCanvas function
+		const { ctx, pixelArray } = getCanvas();
+
+		// Shuffles pixels
+		let pixels = shuffleArray(pixelArray);
+		let iData = [];
+
+		// Converts pixels from 2d to 1d array, saves in iData array
+		for (let i = 0; i < pixels.length; i++) {
+			for (let j = 0; j < pixels[i].length; j++) {
+				iData.push(pixels[i][j]);
 			}
 		}
 
 		// Creates new image data with same dimensions as original rectangle
-		let imgData = ctx.createImageData(150, 100);
+		let imgData = ctx.createImageData(CANVAS_WIDTH, CANVAS_HEIGHT);
 		// Copies over every new pixel
 		for (let i = 0; i < imgData.data.length; i += 4) {
 			imgData.data[i + 0] = iData[i];
@@ -68,14 +100,16 @@ function Colors() {
 			imgData.data[i + 3] = iData[i + 3];
 		}
 		// Replaces image with reversed one
-		ctx.putImageData(imgData, 20, 20);
+		ctx.putImageData(imgData, 0, 0);
 	};
+
+	const sort = () => {};
 
 	return (
 		<div className='color'>
 			<button onClick={draw}>Draw</button>
-			<button onClick={getData}>Get Data</button>
 			<button onClick={rev}>Reverse</button>
+			<button onClick={getCanvas}>Get Data</button>
 			<canvas id='canv'></canvas>
 		</div>
 	);
