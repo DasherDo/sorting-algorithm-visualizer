d957c7b87933b80f3f497eb929b629e581704370
Josh <josh@hessjd.com>
2023-05-03 14:55:12 -0500

    update

--- src/pages/Colors.jsx
+++ src/pages/Colors.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './colors.css';
 
 function Colors() {
-	const CANVAS_WIDTH = 350;
-	const CANVAS_HEIGHT = 350;
+	const CANVAS_WIDTH = 200;
+	const CANVAS_HEIGHT = 200;
 
 	const shuffleArray = (arr) => {
 		for (let i = arr.length - 1; i > 0; i--) {
@@ -37,8 +37,28 @@ function Colors() {
 			}
 			iData.push(cur);
 		}
-		console.log(iData);
 		return { ctx: ctx, pixelArray: iData };
+	};
+
+	const saveCanvas = (ctx, pixelArray) => {
+		let iData = [];
+		for (let i = 0; i < pixelArray.length; i++) {
+			for (let j = 0; j < pixelArray[i].length; j++) {
+				iData.push(pixelArray[i][j]);
+			}
+		}
+
+		// Creates new image data with same dimensions as original rectangle
+		let imgData = ctx.createImageData(CANVAS_WIDTH, CANVAS_HEIGHT);
+		// Copies over every new pixel
+		for (let i = 0; i < imgData.data.length; i += 4) {
+			imgData.data[i + 0] = iData[i];
+			imgData.data[i + 1] = iData[i + 1];
+			imgData.data[i + 2] = iData[i + 2];
+			imgData.data[i + 3] = iData[i + 3];
+		}
+		// Replaces image with reversed one
+		ctx.putImageData(imgData, 0, 0);
 	};
 
 	const draw = () => {
@@ -81,35 +101,51 @@ function Colors() {
 
 		// Shuffles pixels
 		let pixels = shuffleArray(pixelArray);
-		let iData = [];
-
-		// Converts pixels from 2d to 1d array, saves in iData array
-		for (let i = 0; i < pixels.length; i++) {
-			for (let j = 0; j < pixels[i].length; j++) {
-				iData.push(pixels[i][j]);
-			}
-		}
-
-		// Creates new image data with same dimensions as original rectangle
-		let imgData = ctx.createImageData(CANVAS_WIDTH, CANVAS_HEIGHT);
-		// Copies over every new pixel
-		for (let i = 0; i < imgData.data.length; i += 4) {
-			imgData.data[i + 0] = iData[i];
-			imgData.data[i + 1] = iData[i + 1];
-			imgData.data[i + 2] = iData[i + 2];
-			imgData.data[i + 3] = iData[i + 3];
-		}
-		// Replaces image with reversed one
-		ctx.putImageData(imgData, 0, 0);
+		saveCanvas(ctx, pixels);
 	};
 
-	const sort = () => {};
+	const swap = (arr, x, y) => {
+		let tmp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = tmp;
+	};
+
+	const sum = (arr) => {
+		return arr.reduce((x, y) => {
+			return x + y;
+		});
+	};
+
+	const sort = () => {
+		const { ctx, pixelArray } = getCanvas();
+		const len = pixelArray.length;
+		for (let i = 0; i < len - 1; i++) {
+			for (let j = 0; j < len - i - 1; j++) {
+				if (sum(pixelArray[j]) > sum(pixelArray[j + 1])) {
+					let tmp = pixelArray[j];
+					pixelArray[j] = pixelArray[j + 1];
+					pixelArray[j] = tmp;
+				}
+				// Case where sums are equal but rgb values are not
+				// else if (sum(pixelArray[j]) === sum(pixelArray[j + 1])) {
+				// 	if (pixelArray[j][0] > pixelArray[j + 1][0]) {
+				// 		swap(pixelArray, j, j + 1);
+				// 	} else if (pixelArray[j][1] > pixelArray[j + 1][1]) {
+				// 		swap(pixelArray, j, j + 1);
+				// 	} else if (pixelArray[j][2] > pixelArray[j + 1][2]) {
+				// 		swap(pixelArray, j, j + 1);
+				// 	}
+				// }
+			}
+		}
+		saveCanvas(ctx, pixelArray);
+	};
 
 	return (
 		<div className='color'>
 			<button onClick={draw}>Draw</button>
-			<button onClick={rev}>Reverse</button>
-			<button onClick={getCanvas}>Get Data</button>
+			<button onClick={rev}>Shuffle</button>
+			<button onClick={sort}>Sort</button>
 			<canvas id='canv'></canvas>
 		</div>
 	);
